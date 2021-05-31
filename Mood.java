144aa25d99a50fac1a47c73c030696eda3c0aed2
andreawatkins <awatki22@depaul.edu>
2021-05-31 00:17:32 -0500

    Created methods for Mood class, test, and create first mood and example tests, romantic. (Just tests, not using API)

--- Mood.java
+++ Mood.java
@@ -1,3 +1,28 @@
-public abstract class Mood {
-    
+import java.util.ArrayList;
+
+public class Mood {
+    String price;
+    static Romantic romantic = new Romantic();
+
+
+    public  Restaurant getRestaurant(String mood){
+        Restaurant pick = null;
+        if(mood == "Romantic"){
+           pick = romantic.getRomantic();
+
+        }
+        return pick;
+    }
+
+    public static void main(String[] args) {
+
+
+        Mood.romantic.setRestaurant();
+        Restaurant pick = romantic.getRestaurant("Romantic");
+
+        System.out.println(pick.name);
+    }
 }
+
+
+
---
+++ Romantic.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
+
+public class Romantic extends Mood {
+
+    ArrayList<Restaurant> romanticRestaurants = new ArrayList<Restaurant>();
+
+
+    int numRestaurants = romanticRestaurants.size();
+
+    public void setRestaurant() {
+        Restaurant OliveGarden = new Restaurant("2", "OliveGarden", "MKE");
+        Restaurant McDonalds = new Restaurant("3", "McDonalds", "MKE");
+        Restaurant TacoBell = new Restaurant("4", "TacoBell", "MKE");
+        romanticRestaurants.add(OliveGarden);
+        romanticRestaurants.add(McDonalds);
+        romanticRestaurants.add(TacoBell);
+    }
+
+
+    public Restaurant getRomantic() {
+        Random random_index = new Random();
+        int index = random_index.nextInt(romanticRestaurants.size());
+        return (romanticRestaurants.get(index));
+    }
+
+}
