9ac2e7edc3469fc81a6e5598dbf57645ca9fbb04
veeyn <32500013+veeyn@users.noreply.github.com>
2021-06-05 17:39:26 -0400

    Add files via upload

---
+++ uchoose/src/RestaurantOrder.java
@@ -0,0 +1,14 @@
+public class RestaurantOrder{
+    public String orderedItem;
+    public String note;
+    public int rating;
+
+    public RestaurantOrder (String order, String note, int rating ) {
+        this.orderedItem = order;
+        this.note = note;
+        this.rating = rating;
+
+
+    }
+    
+}
